3f940e6920f22df9a9c6a578cbf5136af0c7fc62
Martina Toshevska <martina.tosevska.95@gmail.com>
2018-03-04 15:46:33 +0100

    Add code for parsing region descriptions

--- JSONParser.py
+++ JSONParser.py
@@ -24,6 +24,28 @@ def parse_region_data(file1, file2):
             print(str(i) + '/' + str(len(data)))
 
 
+def parse_region_descriptions(file1, file2):
+    # image_id, region_id, x, y, w, h,
+    with open(file1, 'r', encoding='utf-8') as doc:
+        data = json.loads(doc.read())
+    with open(file2, 'w+', encoding='utf-8') as doc:
+        i = 0
+        for image in data:
+            img_id = image['id']
+            regions = image['regions']
+            for region in regions:
+                x = region['x']
+                y = region['y']
+                w = region['width']
+                h = region['height']
+                reg_id = region['region_id']
+                desc = region['phrase']
+                doc.write(str(img_id) + ',' + str(reg_id) + ',' + str(x) + ',' + str(y) + ',' + str(w) + ',' + str(h) +
+                          ',' + desc + '\n')
+            i += 1
+            print(str(i) + '/' + str(len(data)))
+
+
 def download_images(file):
     directory = 'Visual Genome\\'
     if not os.path.exists(directory):
@@ -43,5 +65,8 @@ def download_images(file):
 
 objects_json = 'objects.json'
 object_labels = 'object_labels.txt'
-parse_region_data(objects_json, object_labels)
+region_attributes = 'region_descriptions.json'
+region_descriptions = 'region_descriptions.txt'
+# parse_region_data(objects_json, object_labels)
 # download_images(objects_json)
+parse_region_descriptions(region_attributes, region_descriptions)
