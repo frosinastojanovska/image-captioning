Correct output for loss calculation

dense_img_cap/preprocess.py
@@ -1,22 +1,29 @@
 import json
-import nltk
 import numpy as np
 from string import punctuation
 from collections import Counter
-from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
 
 def load_corpus(tokens, embeddings, embeddings_dim):
     id_to_word = dict()
     word_to_id = dict()
-    embedding_matrix = np.zeros((len(tokens) + 1, embeddings_dim))
-    id_to_word[0] = '<UNK>'
-    word_to_id['<UNK>'] = 0
+    embedding_matrix = np.zeros((len(tokens) + 3, embeddings_dim))
+    id_to_word[0] = '<unk>'
+    word_to_id['<unk>'] = 0
+
+    # Add <start> and <end> tokens and initialize them randomly in range [-0.5, 0.5]
+    embeddings['<start>'] = np.random.mtrand._rand.rand(embeddings_dim) - 0.5
+    id_to_word[1] = '<start>'
+    word_to_id['<start>'] = 1
+    embeddings['<end>'] = np.random.mtrand._rand.rand(embeddings_dim) - 0.5
+    id_to_word[2] = '<end>'
+    word_to_id['<end>'] = 2
+
     for i in range(len(tokens)):
-        id_to_word[i + 1] = tokens[i]
-        word_to_id[tokens[i]] = i + 1
-        embedding_matrix[i + 1, :] = embeddings[tokens[i]]
+        id_to_word[i + 3] = tokens[i]
+        word_to_id[tokens[i]] = i + 3
+        embedding_matrix[i + 3, :] = embeddings[tokens[i]]
     return word_to_id, id_to_word, embedding_matrix
 
 
@@ -33,25 +40,20 @@ def load_embeddings(file_name):
     return embeddings
 
 
-def tokenize_corpus(data_file, train, validation, embeddings):
+def tokenize_corpus(data_file, train, embeddings):
     with open(data_file, 'r', encoding='utf-8') as doc:
         data = json.loads(doc.read())
     corpus = list()
     for data_point in data:
-        if data_point['id'] in train or data_point['id'] in validation:
+        if data_point['id'] in train:
             regs = data_point['regions']
             for reg in regs:
                 caption = reg['phrase']
                 tokens = word_tokenize(caption.lower())
-                tags = nltk.pos_tag(tokens)
-                for tag in tags:
-                    if tag[1].startswith('NN'):
-                        corpus.append(tag[0])
+                for tag in tokens:
+                    corpus.append(tag)
     frequencies = sorted(Counter(corpus).items(), key=lambda x: x[1], reverse=True)
-    return set([x[0] for x in frequencies if x[1] >= 15 and
-                x[0] in embeddings and
-                x[0] not in punctuation and
-                x[0] not in set(stopwords.words('english'))])
+    return set([x[0] for x in frequencies if x[1] >= 15 and x[0] in embeddings and x[0] not in punctuation])
 
 
 def encode_caption(caption, word_to_id):
@@ -65,6 +67,17 @@ def encode_caption(caption, word_to_id):
     return np.array(vector)
 
 
+def encode_caption_v2(caption, word_to_id):
+    """ convert caption from string to one-hot array """
+    tokens = word_tokenize(caption.lower())
+    vector = list()
+    for token in tokens:
+        encoded_token = encode_word_v2(token, word_to_id)
+        if len(encoded_token) != 0 and encoded_token[0] != 1:
+            vector.append(encoded_token)
+    return np.array(vector)
+
+
 def encode_word(word, word_to_id):
     """ convert word to its index"""
     if word in word_to_id.keys():
@@ -72,6 +85,19 @@ def encode_word(word, word_to_id):
     else:
         pos = 0
     return pos
+
+
+def encode_word_v2(word, word_to_id):
+    """ convert word to its one-hot vector"""
+    if word in word_to_id.keys():
+        pos = word_to_id[word]
+        vec = np.zeros(len(word_to_id))
+        vec[pos] = 1
+    else:
+        pos = word_to_id['<UNK>']
+        vec = np.zeros(len(word_to_id))
+        vec[pos] = 1
+    return vec
 
 
 def decode_caption(vector, id_to_word):

dense_img_cap_separate_models/preprocess.py
@@ -2,7 +2,6 @@ import json
 import numpy as np
 from string import punctuation
 from collections import Counter
-from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
 
@@ -10,8 +9,8 @@ def load_corpus(tokens, embeddings, embeddings_dim):
     id_to_word = dict()
     word_to_id = dict()
     embedding_matrix = np.zeros((len(tokens) + 3, embeddings_dim))
-    id_to_word[0] = ''
-    word_to_id[''] = 0
+    id_to_word[0] = '<unk>'
+    word_to_id['<unk>'] = 0
 
     # Add <start> and <end> tokens and initialize them randomly in range [-0.5, 0.5]
     embeddings['<start>'] = np.random.mtrand._rand.rand(embeddings_dim) - 0.5
@@ -54,9 +53,7 @@ def tokenize_corpus(data_file, train, embeddings):
                 for tag in tokens:
                     corpus.append(tag)
     frequencies = sorted(Counter(corpus).items(), key=lambda x: x[1], reverse=True)
-    return set([x[0] for x in frequencies if x[1] >= 15 and x[0] in embeddings and x[0] not in punctuation and
-                x[0] not in set(stopwords.words('english'))])
-    # return set([x[0] for x in frequencies if x[0] in embeddings])
+    return set([x[0] for x in frequencies if x[1] >= 15 and x[0] in embeddings and x[0] not in punctuation])
 
 
 def encode_caption(caption, word_to_id):

dense_img_cap_separate_models/text_generation_model.py
@@ -104,11 +104,12 @@ class VisualGenomeDataset(Dataset):
         rois = []
         caps = []
         for roi, caption in zip(image_info['rois'], image_info['captions']):
-            caption_modified = '<start>' + caption[0] + '<end>' if len(caption[0]) < self.padding_size - 2 \
-                else '<start>' + caption[0][:8] + '<end>'
-            cap = self.encode_region_caption(caption_modified)
+            cap = self.encode_region_caption(caption[0])
             if cap.size != 0:
                 rois.append(roi)
+                # add <start> and <end> tokens
+                cap = np.hstack((np.array(1), cap, np.array(2))) if len(cap) < (self.padding_size - 2) \
+                    else np.hstack((np.array(1), cap[:(self.padding_size - 2)], np.array(2)))
                 caps.append(cap)
         captions = pad_sequences(caps, maxlen=self.padding_size, padding='post', dtype='float').astype(np.float32)
         rois = np.array(rois)
@@ -156,6 +157,17 @@ def word_generation_model(features_input, lstm_units, config):
 
 
 def build_roi_caption_model_training(features_input, lstm_units, config):
+    """ Model for training ROI caption generation
+
+    :param features_input: ROI features shape
+    :type features_input: list(int)
+    :param lstm_units: size of the units of LSTM cells
+    :type lstm_units: int
+    :param config: configuration object
+    :type config: DenseCapConfig object
+    :return: roi caption generation model
+    :rtype: KM.Model object
+    """
     inputs = KL.Input(batch_shape=[config.BATCH_SIZE] + features_input, name="input_imgcap_caption_features")
     feature = KL.Lambda(lambda x: x[:, :, :-config.PADDING_SIZE], name="imgcap_caption_feature")(inputs)
     gt_captions = KL.Lambda(lambda x: x[:, :, -config.PADDING_SIZE:], name="imgcap_caption_gt_captions")(inputs)
@@ -178,6 +190,8 @@ def build_roi_caption_model_training(features_input, lstm_units, config):
 
 
 class ROICaptionInferenceLayer(KL.Layer):
+    """ Layer for generating ROI caption"""
+
     def __init__(self, word_model, config):
         super(ROICaptionInferenceLayer, self).__init__()
         self.word_model = word_model
@@ -326,7 +340,7 @@ def data_generator(dataset, features_model, config, batch_size, shuffle=False):
             prev_im_id = image_id
             input_words = cap
             output_words = []
-            for c in cap:
+            for c in np.append(cap, [0.0])[1:]:
                 word = np.zeros(config.VOCABULARY_SIZE)
                 word[int(c)] = 1
                 output_words.append(word)
@@ -362,9 +376,9 @@ if __name__ == '__main__':
     test_image_ids = image_ids_list[100000:]
 
     # load word ids
-    id_to_word_file = '../dataset/id_to_word.pickle'
-    word_to_id_file = '../dataset/word_to_id.pickle'
-    embedding_matrix_file = '../dataset/embedding_matrix.pickle'
+    id_to_word_file = '../dataset/dense_img_cap/id_to_word.pickle'
+    word_to_id_file = '../dataset/dense_img_cap/word_to_id.pickle'
+    embedding_matrix_file = '../dataset/dense_img_cap/embedding_matrix.pickle'
     train_rois_file = '../dataset/train_rois.pickle'
     val_rois_file = '../dataset/val_rois.pickle'
 